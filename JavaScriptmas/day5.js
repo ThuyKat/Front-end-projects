8ea226bfbfad1e4284ad279acbdd707e10566131
ThuyKat <katiefirst7@gmail.com>
2024-12-06 15:52:58 +1100

    password generator project

---
+++ JavaScriptmas/day5.js
@@ -0,0 +1,4 @@
+
+el = ["Can Assault", "Santa Claus"];
+let el1 = el[0].toLowerCase().split("").filter(char=>char !== ' ').sort();
+console.log(el1);
---
+++ Password Generator/index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+//variables
+let chars=[];
+//get the DOM elements
+let generatePasswordEl = document.getElementById("btn-generate-password");
+let pass1El = document.getElementsByClassName("password")[0];
+let pass2El = document.getElementsByClassName("password")[1];
+
+//password must be 13 character length
+generatePasswordEl.addEventListener("click",generatePassword);
+function generateRandomString(){
+    console.log("generating password");   
+    for(let i=0;i<13;i++){
+        let randomIndex = Math.floor(Math.random()*characters.length);
+        chars.push(characters[randomIndex]);
+    }
+    return chars.join("")
+}
+
+function generatePassword(){
+    pass1El.textContent = generateRandomString();
+    chars.length = 0;
+    pass2El.textContent = generateRandomString();
+}
+
+
+
