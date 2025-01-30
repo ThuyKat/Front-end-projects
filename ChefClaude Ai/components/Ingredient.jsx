b915345df53b5f17c138885df801e01d90921a5d
ThuyKat <katiefirst7@gmail.com>
2025-01-30 16:14:30 +1100

    added Chef Claude project

---
+++ ChefClaude Ai/components/Ingredient.jsx
@@ -0,0 +1,7 @@
+export default function Ingredient(props){
+    return(
+        <li>
+            {props.ingredient}
+        </li>
+    )
+}
---
+++ ChefClaude Ai/components/Main.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Ingredient from "./Ingredient";
+export default function Main() {
+  const[recipe,setRecipe] = useState("")
+  const [ingredients, setIngredients] = useState([
+    "chicken",
+    "corn",
+    "heavy cream",
+  ]);
+
+  const ingredientList = ingredients.map((item) => (
+    <Ingredient key={item} ingredient={item} />
+  ));
+  function addIngredient(formData) {
+    const newIngredient = formData.get("new-ingredient");
+    setIngredients((prevIngredient) => [...prevIngredient, newIngredient]);
+    // console.log(Object.fromEntries(formData));
+  }
+  function getRecipe(){
+    setRecipe("this is recipe")
+  }
+
+  return (
+    <main className="main-container">
+
+      <form action={addIngredient} className="new-ingredient-form">
+        <input type="text" placeholder="e.g. oregano" name="new-ingredient" aria-label="Add ingredient" />
+        <button type="submit">+ Add ingredient</button>
+      </form>
+
+      <section>
+      <h2>Ingredients on hand</h2>
+      <div className="ingredients-container" aria-live="polite">
+        <ul>{ingredientList}</ul>
+      </div>
+      <div class="get-recipe-container">
+        <div>
+            <h3>Ready for a recipe</h3>
+            <p>Generate a recipe from your list of ingredients</p>
+        </div>
+        <button onClick={getRecipe}>Get a recipe</button>
+      </div>
+      <div class="recipe-container">
+            {recipe}
+      </div>
+      </section>
+    </main>
+  );
+}
