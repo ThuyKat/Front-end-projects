3b4b35f7c50c600538d277a2b27bebed3ede21a2
ThuyKat <katiefirst7@gmail.com>
2025-05-22 23:56:30 +1000

    useNavigate to redirect user back to /host

--- VanLife/src/pages/Login.jsx
+++ VanLife/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import {useState, useRef} from "react"
-import {useLocation} from "react-router-dom"
+import {useLocation, useNavigate} from "react-router-dom"
 import {loginUser} from "../api"
 
 export default function Login() {
 const [state, setState] = useState("idle")
+const [error, setError] = useState(null)
 const location = useLocation()
+const navigate = useNavigate()
 const [formData,setFormData] = useState({
     email:"",
     password:""
@@ -22,17 +24,25 @@ const handleInputChange=(e)=>{
 const handleSubmit= async(formData)=>{
     setState("submitting")
     const loginData = Object.fromEntries(formData)
+    loginBtn.current.disabled = true
+    loginBtn.current.style.backgroundColor="grey"
+
     loginUser(loginData).then(data => {
         setState("idle")
-        loginBtn.current.disabled = true
-        loginBtn.current.style.backgroundColor="grey"
-        console.log(data)})
+        console.log(data)
+        if(data.status === 200){
+            setError(null)
+            navigate("/home")
+        }
+    
+    }).catch(err=>setError(err))
 
 }
   return (
     <div className="login-container">
         {location.state?.message && <h3 className="error-message">{location.state.message}</h3>}
         <h1>Sign in to your account</h1>
+        {error? <p>{error}</p>:null}
         <form action={handleSubmit}>
             <div className="input-group">
                 <input placeholder="Email address" type="email" name="email" value={formData.email} onChange={handleInputChange}/>
